fa9b5c7e9aa55c16a76eee2393f36886d2360e5c
Stephen <stephen.frx@gmail.com>
2016-04-17 13:20:55 +0800

    Add flash

--- resources/views/layouts/_flash.blade.php
+++ resources/views/layouts/_flash.blade.php
@@ -1,10 +1,12 @@
-@if (session('status'))
-<!-- Status -->
+@if (session('success'))
+<!-- Success -->
 <section class="row">
     <div class="col-sm-12">
-        <div class="alert alert-success alert-dismissable">
+        <div class="alert alert-success alert-dismissable" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            {{ session('status') }}
+            <span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
+            <span class="sr-only">Success:</span>
+            {{ session('success') }}
         </div>
     </div>
     <!-- /.col-sm-12 -->
@@ -12,13 +14,47 @@
 <!-- /.row -->
 @endif
 
-@if (isset($info))
+@if (session('info'))
 <!-- Information -->
 <section class="row">
     <div class="col-sm-12">
-        <div class="alert alert-info alert-dismissable">
+        <div class="alert alert-info alert-dismissable" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            {{ $info }}
+            <span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>
+            <span class="sr-only">Information:</span>
+            {{ session('info') }}
+        </div>
+    </div>
+    <!-- /.col-sm-12 -->
+</section>
+<!-- /.row -->
+@endif
+
+@if (session('warning'))
+<!-- Warning -->
+<section class="row">
+    <div class="col-sm-12">
+        <div class="alert alert-warning alert-dismissable" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+            <span class="sr-only">Warning:</span>
+            {{ session('warning') }}
+        </div>
+    </div>
+    <!-- /.col-sm-12 -->
+</section>
+<!-- /.row -->
+@endif
+
+@if (session('error'))
+<!-- Error -->
+<section class="row">
+    <div class="col-sm-12">
+        <div class="alert alert-danger alert-dismissable" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>
+            <span class="sr-only">Error:</span>
+            {{ session('error') }}
         </div>
     </div>
     <!-- /.col-sm-12 -->
@@ -32,7 +68,8 @@
     <div class="col-sm-12">
         <div class="alert alert-danger alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <strong>注意：出错啦！</strong>
+            <span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>
+            <span class="sr-only">Error:</span>
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -275,52 +275,7 @@
 
             <!-- Page wrapper -->
             <main id="page-wrapper">
-                @if (session('status'))
-                <!-- Status -->
-                <section class="row">
-                    <div class="col-sm-12">
-                        <div class="alert alert-success alert-dismissable">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            {{ session('status') }}
-                        </div>
-                    </div>
-                    <!-- /.col-sm-12 -->
-                </section>
-                <!-- /.row -->
-                @endif
-
-                @if (isset($info))
-                <!-- Information -->
-                <section class="row">
-                    <div class="col-sm-12">
-                        <div class="alert alert-info alert-dismissable">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            {{ $info }}
-                        </div>
-                    </div>
-                    <!-- /.col-sm-12 -->
-                </section>
-                <!-- /.row -->
-                @endif
-
-                @if ($errors->any())
-                <!-- Errors -->
-                <section class="row">
-                    <div class="col-sm-12">
-                        <div class="alert alert-danger alert-dismissable">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            <strong>注意：出错啦！</strong>
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- /.col-sm-12 -->
-                </section>
-                <!-- /.row -->
-                @endif
+                @include('layouts._flash')
 
                 <article class="row">
                     <header class="col-sm-12">
