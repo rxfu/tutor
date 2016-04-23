08786027db29bf62438628ec9127c20e3e8fb37b
Stephen <stephen.frx@gmail.com>
2016-04-23 11:41:46 +0800

    Add home controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,8 +9,16 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+	return redirect('home');
+});
+
+Route::group(['middleware' => ['web']], function () {
+	Route::auth();
+
+	Route::group(['middelware' => ['auth']], function () {
+		Route::get('dashboard', ['as' => 'home', 'uses' => 'HomeController@index']);
+	});
 });
