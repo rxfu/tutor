a5794aec4d6e238d843fdf6f3a09331f0ddc959e
Stephen Foo <stephen.frx@gmail.com>
2016-04-29 23:00:29 +0800

    Fixed role create

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -9,21 +9,43 @@
 	{{ csrf_field() }}
 	<fieldset>
 		<div class="form-group">
-			<label for="slug" class="control-label col-sm-2">标识符</label>
+			<label for="username" class="control-label col-sm-2">用户名</label>
 			<div class="col-sm-10">
-				<input type="text" name="slug" id="slug" class="form-control" placeholder="标识符" value="{{ old('slug') }}">
+				<input type="text" name="username" id="username" class="form-control" placeholder="用户名" value="{{ old('username') }}">
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="name" class="control-label col-sm-2">名称</label>
+			<label for="xm" class="control-label col-sm-2">姓名</label>
 			<div class="col-sm-10">
-				<input type="text" name="name" id="name" class="form-control" placeholder="名称" value="{{ old('name') }}">
+				<input type="text" name="xm" id="xm" class="form-control" placeholder="姓名" value="{{ old('xm') }}">
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="description" class="control-label col-sm-2">描述</label>
+			<label for="sfzh" class="control-label col-sm-2">身份证号</label>
 			<div class="col-sm-10">
-				<textarea name="description" cols="50" rows="10" class="form-control" placeholder="描述">{{ old('description') }}</textarea>
+				<input type="text" name="sfzh" id="sfzh" class="form-control" placeholder="身份证号" value="{{ old('sfzh') }}">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="role" class="control-label col-sm-2">所属角色</label>
+			<div class="col-sm-10">
+				@inject('roles', 'Tis\Account\Repositories\RoleRepository')
+				<select name="role" class="form-control">
+					@foreach ($roles->getAll() as $role)
+						<option value="{{ $role->id }}"{{ old('role') === $role->id ? ' selected' : '' }}>{{ $role->name }}</option>
+					@endforeach
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="is_super" class="control-label col-sm-2">是否超级管理员</label>
+			<div class="col-sm-10">
+				<label class="radio-inline">
+					<input type="radio" name="is_super" value="1" checked>&nbsp;是
+				</label>
+				<label class="radio-inline">
+					<input type="radio" name="is_super" value="0">&nbsp;否
+				</label>
 			</div>
 		</div>
 		<div class="col-sm-offset-2 col-sm-10">
