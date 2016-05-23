b6eebb41c317ec7000f81f011518e450f24624e1
Stephen <stephen.frx@gmail.com>
2016-05-23 22:01:02 +0800

    Add statistics service

---
+++ app/Tis/Tutor/Services/StatisticsService.php
@@ -0,0 +1,28 @@
+<?php
+
+class StatisticsService {
+
+	public function statBygender() {
+
+	}
+
+	public function statByCollege() {
+
+	}
+
+	public function statByDegree() {
+
+	}
+
+	public function statByPosition() {
+
+	}
+
+	public function statByDiscipline() {
+
+	}
+
+	public function statBySubdiscipline() {
+
+	}
+}
