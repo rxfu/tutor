3c997121f401ed5d0a6de34e9a35b49b10e0c0f3
Stephen <stephen.frx@gmail.com>
2016-07-04 22:56:57 +0800

    Fixed statistics display

--- app/Tis/Tutor/Services/StatisticsService.php
+++ app/Tis/Tutor/Services/StatisticsService.php
@@ -7,34 +7,10 @@ use Illuminate\Support\Facades\DB;
 class StatisticsService {
 
 	public function countByGender() {
-		$query   = "SELECT xb, SUM(IF(age < 30, 1, 0)) AS 'under_30', SUM(IF(age BETWEEN 30 AND 34, 1, 0)) AS 'to_34', SUM(IF(age BETWEEN 35 AND 39, 1, 0)) AS 'to_39', SUM(IF(age BETWEEN 40 AND 44, 1, 0)) AS 'to_44', SUM(IF(age BETWEEN 45 AND 49, 1, 0)) AS 'to_49', SUM(IF(age BETWEEN 50 AND 54, 1, 0)) AS 'to_54', SUM(IF(age BETWEEN 55 AND 59, 1, 0)) AS 'to_59', SUM(IF(age BETWEEN 60 AND 64, 1, 0)) AS 'to_64', SUM(IF(age >= 65, 1, 0)) AS 'over_65', COUNT(*) AS total FROM (SELECT xb, TIMESTAMPDIFF(YEAR, csny, CURDATE()) AS age FROM y_ds_dsxx) AS derived GROUP BY xb ORDER BY xb";
+		$query   = "SELECT xb, mc, SUM(IF(age < 30, 1, 0)) AS 'under_30', SUM(IF(age BETWEEN 30 AND 34, 1, 0)) AS 'to_34', SUM(IF(age BETWEEN 35 AND 39, 1, 0)) AS 'to_39', SUM(IF(age BETWEEN 40 AND 44, 1, 0)) AS 'to_44', SUM(IF(age BETWEEN 45 AND 49, 1, 0)) AS 'to_49', SUM(IF(age BETWEEN 50 AND 54, 1, 0)) AS 'to_54', SUM(IF(age BETWEEN 55 AND 59, 1, 0)) AS 'to_59', SUM(IF(age BETWEEN 60 AND 64, 1, 0)) AS 'to_64', SUM(IF(age >= 65, 1, 0)) AS 'over_65', COUNT(*) AS total FROM (SELECT xb, TIMESTAMPDIFF(YEAR, csny, CURDATE()) AS age FROM y_ds_dsxx) AS derived INNER JOIN y_xb ON xb = dm GROUP BY xb, mc ORDER BY xb";
 		$results = DB::select($query);
 
-		$counts = [];
-		foreach ($results as $result) {
-			$counts[0]['title']     = '30岁以下';
-			$counts[0][$result->xb] = $result->under_30;
-			$counts[1]['title']     = '30~34岁';
-			$counts[1][$result->xb] = $result->to_34;
-			$counts[2]['title']     = '35~39岁';
-			$counts[2][$result->xb] = $result->to_39;
-			$counts[3]['title']     = '40~44岁';
-			$counts[3][$result->xb] = $result->to_44;
-			$counts[4]['title']     = '45~49岁';
-			$counts[4][$result->xb] = $result->to_49;
-			$counts[5]['title']     = '50~54岁';
-			$counts[5][$result->xb] = $result->to_54;
-			$counts[6]['title']     = '55~59岁';
-			$counts[6][$result->xb] = $result->to_59;
-			$counts[7]['title']     = '60~64岁';
-			$counts[7][$result->xb] = $result->to_64;
-			$counts[8]['title']     = '65岁以上';
-			$counts[8][$result->xb] = $result->over_65;
-			$counts[9]['title']     = '合计';
-			$counts[9][$result->xb] = $result->total;
-		}
-
-		return $counts;
+		return collect($results);
 	}
 
 	public function countByCollege() {
@@ -48,93 +24,21 @@ class StatisticsService {
 		$query   = "SELECT zgxw, SUM(IF(age < 30, 1, 0)) AS 'under_30', SUM(IF(age BETWEEN 30 AND 34, 1, 0)) AS 'to_34', SUM(IF(age BETWEEN 35 AND 39, 1, 0)) AS 'to_39', SUM(IF(age BETWEEN 40 AND 44, 1, 0)) AS 'to_44', SUM(IF(age BETWEEN 45 AND 49, 1, 0)) AS 'to_49', SUM(IF(age BETWEEN 50 AND 54, 1, 0)) AS 'to_54', SUM(IF(age BETWEEN 55 AND 59, 1, 0)) AS 'to_59', SUM(IF(age BETWEEN 60 AND 64, 1, 0)) AS 'to_64', SUM(IF(age >= 65, 1, 0)) AS 'over_65', COUNT(*) AS total FROM (SELECT zgxw, TIMESTAMPDIFF(YEAR, csny, CURDATE()) AS age FROM y_ds_dsxx) AS derived GROUP BY zgxw ORDER BY zgxw";
 		$results = DB::select($query);
 
-		$counts = [];
-		foreach ($results as $result) {
-			$counts[0]['title']       = '30岁以下';
-			$counts[0][$result->zgxw] = $result->under_30;
-			$counts[1]['title']       = '30~34岁';
-			$counts[1][$result->zgxw] = $result->to_34;
-			$counts[2]['title']       = '35~39岁';
-			$counts[2][$result->zgxw] = $result->to_39;
-			$counts[3]['title']       = '40~44岁';
-			$counts[3][$result->zgxw] = $result->to_44;
-			$counts[4]['title']       = '45~49岁';
-			$counts[4][$result->zgxw] = $result->to_49;
-			$counts[5]['title']       = '50~54岁';
-			$counts[5][$result->zgxw] = $result->to_54;
-			$counts[6]['title']       = '55~59岁';
-			$counts[6][$result->zgxw] = $result->to_59;
-			$counts[7]['title']       = '60~64岁';
-			$counts[7][$result->zgxw] = $result->to_64;
-			$counts[8]['title']       = '65岁以上';
-			$counts[8][$result->zgxw] = $result->over_65;
-			$counts[9]['title']       = '合计';
-			$counts[9][$result->zgxw] = $result->total;
-		}
-
-		return $counts;
+		return collect($results);
 	}
 
 	public function countByPosition() {
-		$query   = "SELECT zyzc, SUM(IF(age < 30, 1, 0)) AS 'under_30', SUM(IF(age BETWEEN 30 AND 34, 1, 0)) AS 'to_34', SUM(IF(age BETWEEN 35 AND 39, 1, 0)) AS 'to_39', SUM(IF(age BETWEEN 40 AND 44, 1, 0)) AS 'to_44', SUM(IF(age BETWEEN 45 AND 49, 1, 0)) AS 'to_49', SUM(IF(age BETWEEN 50 AND 54, 1, 0)) AS 'to_54', SUM(IF(age BETWEEN 55 AND 59, 1, 0)) AS 'to_59', SUM(IF(age BETWEEN 60 AND 64, 1, 0)) AS 'to_64', SUM(IF(age >= 65, 1, 0)) AS 'over_65', COUNT(*) AS total FROM (SELECT zyzc, TIMESTAMPDIFF(YEAR, csny, CURDATE()) AS age FROM y_ds_dsxx) AS derived GROUP BY zyzc ORDER BY zyzc";
+		$query   = "SELECT zyzc, mc, SUM(IF(age < 30, 1, 0)) AS 'under_30', SUM(IF(age BETWEEN 30 AND 34, 1, 0)) AS 'to_34', SUM(IF(age BETWEEN 35 AND 39, 1, 0)) AS 'to_39', SUM(IF(age BETWEEN 40 AND 44, 1, 0)) AS 'to_44', SUM(IF(age BETWEEN 45 AND 49, 1, 0)) AS 'to_49', SUM(IF(age BETWEEN 50 AND 54, 1, 0)) AS 'to_54', SUM(IF(age BETWEEN 55 AND 59, 1, 0)) AS 'to_59', SUM(IF(age BETWEEN 60 AND 64, 1, 0)) AS 'to_64', SUM(IF(age >= 65, 1, 0)) AS 'over_65', COUNT(*) AS total FROM (SELECT zyzc, TIMESTAMPDIFF(YEAR, csny, CURDATE()) AS age FROM y_ds_dsxx) AS derived INNER JOIN y_dszc ON zyzc = dm GROUP BY zyzc, mc ORDER BY zyzc";
 		$results = DB::select($query);
 
-		$counts = [];
-		foreach ($results as $result) {
-			$counts[0]['title']       = '30岁以下';
-			$counts[0][$result->zyzc] = $result->under_30;
-			$counts[1]['title']       = '30~34岁';
-			$counts[1][$result->zyzc] = $result->to_34;
-			$counts[2]['title']       = '35~39岁';
-			$counts[2][$result->zyzc] = $result->to_39;
-			$counts[3]['title']       = '40~44岁';
-			$counts[3][$result->zyzc] = $result->to_44;
-			$counts[4]['title']       = '45~49岁';
-			$counts[4][$result->zyzc] = $result->to_49;
-			$counts[5]['title']       = '50~54岁';
-			$counts[5][$result->zyzc] = $result->to_54;
-			$counts[6]['title']       = '55~59岁';
-			$counts[6][$result->zyzc] = $result->to_59;
-			$counts[7]['title']       = '60~64岁';
-			$counts[7][$result->zyzc] = $result->to_64;
-			$counts[8]['title']       = '65岁以上';
-			$counts[8][$result->zyzc] = $result->over_65;
-			$counts[9]['title']       = '合计';
-			$counts[9][$result->zyzc] = $result->total;
-		}
-
-		return $counts;
+		return collect($results);
 	}
 
 	public function countByDiscipline() {
-		$query   = "SELECT yjxkdm, SUM(IF(age < 30, 1, 0)) AS 'under_30', SUM(IF(age BETWEEN 30 AND 34, 1, 0)) AS 'to_34', SUM(IF(age BETWEEN 35 AND 39, 1, 0)) AS 'to_39', SUM(IF(age BETWEEN 40 AND 44, 1, 0)) AS 'to_44', SUM(IF(age BETWEEN 45 AND 49, 1, 0)) AS 'to_49', SUM(IF(age BETWEEN 50 AND 54, 1, 0)) AS 'to_54', SUM(IF(age BETWEEN 55 AND 59, 1, 0)) AS 'to_59', SUM(IF(age BETWEEN 60 AND 64, 1, 0)) AS 'to_64', SUM(IF(age >= 65, 1, 0)) AS 'over_65', COUNT(*) AS total FROM (SELECT yjxkdm, TIMESTAMPDIFF(YEAR, csny, CURDATE()) AS age FROM y_ds_dsxx) AS derived GROUP BY yjxkdm ORDER BY yjxkdm";
+		$query   = "SELECT yjxkdm, mc, SUM(IF(age < 30, 1, 0)) AS 'under_30', SUM(IF(age BETWEEN 30 AND 34, 1, 0)) AS 'to_34', SUM(IF(age BETWEEN 35 AND 39, 1, 0)) AS 'to_39', SUM(IF(age BETWEEN 40 AND 44, 1, 0)) AS 'to_44', SUM(IF(age BETWEEN 45 AND 49, 1, 0)) AS 'to_49', SUM(IF(age BETWEEN 50 AND 54, 1, 0)) AS 'to_54', SUM(IF(age BETWEEN 55 AND 59, 1, 0)) AS 'to_59', SUM(IF(age BETWEEN 60 AND 64, 1, 0)) AS 'to_64', SUM(IF(age >= 65, 1, 0)) AS 'over_65', COUNT(*) AS total FROM (SELECT yjxkdm, TIMESTAMPDIFF(YEAR, csny, CURDATE()) AS age FROM y_ds_dsxx) AS derived INNER JOIN y_yjxk ON yjxkdm = dm GROUP BY yjxkdm, mc ORDER BY yjxkdm";
 		$results = DB::select($query);
 
-		$counts = [];
-		foreach ($results as $result) {
-			$counts[0]['title']         = '30岁以下';
-			$counts[0][$result->yjxkdm] = $result->under_30;
-			$counts[1]['title']         = '30~34岁';
-			$counts[1][$result->yjxkdm] = $result->to_34;
-			$counts[2]['title']         = '35~39岁';
-			$counts[2][$result->yjxkdm] = $result->to_39;
-			$counts[3]['title']         = '40~44岁';
-			$counts[3][$result->yjxkdm] = $result->to_44;
-			$counts[4]['title']         = '45~49岁';
-			$counts[4][$result->yjxkdm] = $result->to_49;
-			$counts[5]['title']         = '50~54岁';
-			$counts[5][$result->yjxkdm] = $result->to_54;
-			$counts[6]['title']         = '55~59岁';
-			$counts[6][$result->yjxkdm] = $result->to_59;
-			$counts[7]['title']         = '60~64岁';
-			$counts[7][$result->yjxkdm] = $result->to_64;
-			$counts[8]['title']         = '65岁以上';
-			$counts[8][$result->yjxkdm] = $result->over_65;
-			$counts[9]['title']         = '合计';
-			$counts[9][$result->yjxkdm] = $result->total;
-		}
-
-		return $counts;
+		return collect($results);
 	}
 
 	public function countBySubdiscipline() {
--- resources/views/statistics/by_age.blade.php
+++ resources/views/statistics/by_age.blade.php
@@ -2,45 +2,66 @@
 
 @section('content')
 <div class="table-responsive">
-    <table id="by-age-table" class="table table-bordered table-striped table-hover">
+    <table id="by-age-table" class="table table-bordered table-striped">
         <thead>
             <tr>
-                <th class="active" rowspan="2">年龄段</th>
-
-                @foreach ($types as $type)
-                    <th class="active" colspan="2">{{ $type->mc }}</th>
-                @endforeach
-
+                <th class="active" colspan="2">年龄段</th>
+                <th class="active">30岁以下</th>
+                <th class="active">30~34岁</th>
+                <th class="active">35~39岁</th>
+                <th class="active">40~44岁</th>
+                <th class="active">45~49岁</th>
+                <th class="active">50~54岁</th>
+                <th class="active">55~59岁</th>
+                <th class="active">60~64岁</th>
+                <th class="active">65岁以上</th>
                 <th class="active">合计</th>
-            </tr>
-            <tr>
-                @foreach ($types as $type)
-                    <th class="active">人数</th>
-                    <th class="active">比例</th>
-                @endforeach
-
-                <th class="active">人数</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($items as $item)
                 <tr>
-                    <?php $total = 0?>
-                    @foreach ($types as $type)
-                        <?php $count{$type->dm} = isset($item[$type->dm]) ? $item[$type->dm] : 0?>
-                        <?php $total += $count{$type->dm}?>
-                    @endforeach
-
-                    <th>{{ $item['title'] }}</th>
-
-                    @foreach ($types as $type)
-                        <td>{{ $count{$type->dm} }}</td>
-                        <td>{{ $total ? round($count{$type->dm} / $total * 100, 2) : $total }}%</td>
-                    @endforeach
-
-                    <td>{{ $total }}</td>
+                    <th class="active" rowspan="2">{{ $item->mc }}</th>
+                    <th class="active">人数</th>
+                    <td>{{ $item->under_30 }}</td>
+                    <td>{{ $item->to_34 }}</td>
+                    <td>{{ $item->to_39 }}</td>
+                    <td>{{ $item->to_44 }}</td>
+                    <td>{{ $item->to_49 }}</td>
+                    <td>{{ $item->to_54 }}</td>
+                    <td>{{ $item->to_59 }}</td>
+                    <td>{{ $item->to_64 }}</td>
+                    <td>{{ $item->over_65 }}</td>
+                    <td>{{ $item->total }}</td>
+                </tr>
+                <tr>
+                    <th class="active">比例</th>
+                    <td>{{ $items->sum('under_30') ? round($item->under_30 / $items->sum('under_30') * 100, 2) : 0 }}%</td>
+                    <td>{{ $items->sum('to_34') ? round($item->to_34 / $items->sum('to_34') * 100, 2) : 0 }}%</td>
+                    <td>{{ $items->sum('to_39') ? round($item->to_39 / $items->sum('to_39') * 100, 2) : 0 }}%</td>
+                    <td>{{ $items->sum('to_44') ? round($item->to_44 / $items->sum('to_44') * 100, 2) : 0 }}%</td>
+                    <td>{{ $items->sum('to_49') ? round($item->to_49 / $items->sum('to_49') * 100, 2) : 0 }}%</td>
+                    <td>{{ $items->sum('to_54') ? round($item->to_54 / $items->sum('to_54') * 100, 2) : 0 }}%</td>
+                    <td>{{ $items->sum('to_59') ? round($item->to_59 / $items->sum('to_59') * 100, 2) : 0 }}%</td>
+                    <td>{{ $items->sum('to_64') ? round($item->to_64 / $items->sum('to_64') * 100, 2) : 0 }}%</td>
+                    <td>{{ $items->sum('over_65') ? round($item->over_65 / $items->sum('over_65') * 100, 2) : 0 }}%</td>
+                    <td>{{ $items->sum('total') ? round($item->total / $items->sum('total') * 100, 2) : 0 }}%</td>
                 </tr>
             @endforeach
+            <tr>
+                <th class="active">合计</th>
+                <th class="active">人数</th>
+                <td>{{ $items->sum('under_30') }}</td>
+                <td>{{ $items->sum('to_34') }}</td>
+                <td>{{ $items->sum('to_39') }}</td>
+                <td>{{ $items->sum('to_44') }}</td>
+                <td>{{ $items->sum('to_49') }}</td>
+                <td>{{ $items->sum('to_54') }}</td>
+                <td>{{ $items->sum('to_59') }}</td>
+                <td>{{ $items->sum('to_64') }}</td>
+                <td>{{ $items->sum('over_65') }}</td>
+                <td>{{ $items->sum('total') }}</td>
+            </tr>
         </tbody>
     </table>
 </div>
